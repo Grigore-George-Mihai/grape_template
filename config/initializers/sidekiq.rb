5c023eeef7bc6f437db39384af4ced0175cfa19c
George Mihai Grigore <grigore.george.mihaii@gmail.com>
2024-09-29 15:25:45 +0300

    Mount Sidekiq with session support and basic auth

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,8 @@ sidekiq_config = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
 
-  config.options[:concurrency] = 5
-  config.options[:queues] = ['critical', 'default', 'mailers']
+  config.concurrency = 5
+  config.queues = ['critical', 'default', 'mailers']
 end
 
 Sidekiq.configure_client do |config|
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
+require 'sidekiq/web'
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  username == ENV.fetch("SIDEKIQ_USERNAME", "admin") && password == ENV.fetch("SIDEKIQ_PASSWORD", "password")
+end
+
 Rails.application.routes.draw do
-  mount ApiRoot => '/'
+  mount ApiRoot => "/"
 
   if Rails.env.development? || Rails.env.staging?
-    mount GrapeSwaggerRails::Engine => '/swagger'
+    mount GrapeSwaggerRails::Engine => "/swagger"
+  end
+
+  if Rails.env.development? || Rails.env.production?
+    Sidekiq::Web.use ActionDispatch::Cookies
+    Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: ENV.fetch("SIDEKIQ_SESSION_KEY", "_your_app_session")
+    mount Sidekiq::Web => "/sidekiq"
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +24,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # root "posts#index"
 end
